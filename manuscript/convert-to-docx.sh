21b99acef186f1592c880b2ec05488d8fb33180b
Tuan Pham <42875763+tuanpham96@users.noreply.github.com>
2022-06-14 17:53:39 -0500

    Move necessary files from docx branch

--- manuscript/convert-to-docx.sh
+++ manuscript/convert-to-docx.sh
@@ -1,15 +1,14 @@
-VERSION="$(echo $(git rev-parse --abbrev-ref HEAD) | sed 's/-docx//')"
-echo "$VERSION"
- 
-cp main.pdf manuscript-PhamHansel-"$VERSION".pdf
+# VERSION="$(echo $(git rev-parse --abbrev-ref HEAD) | sed 's/-docx//')"
+# echo "$VERSION"
+
+cp main.pdf manuscript-PhamHansel.pdf
 
 find . \
   -type d \( -path ./misc -o \
 			-path ./archive -o \
-			-path ./cmts -o \
-			-path ./plos -o \
-			-path ./version \
-			-o -path ./pandoc-resources \) -prune -o \
+			-path ./output -o \
+			-path ./clean-bib -o \
+			-path ./resources \) -prune -o \
   -name '*.tex' \
   -exec sed -i -E\
 	-e 's/\\autoref\{fig([^\{]*)\}/\\textbf{Fig. \\ref{fig\1}}/g' \
@@ -22,7 +21,7 @@ find figures -type f -name 'Fig*.tex' \
 	-e 's/\\(HLCh|HLChFig)//g' \
 	{} \;
 
-DOCX_FILE=manuscript-PhamHansel-"$VERSION".docx
+DOCX_FILE=manuscript-PhamHansel.docx
 pandoc --defaults front.yml \
 	-s main.tex \
 	-o "$DOCX_FILE"
