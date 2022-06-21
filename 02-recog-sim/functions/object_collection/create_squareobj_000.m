e3fa50a088187edd6a9545266bfd6d8b5f2f8326
Tuan Pham <42875763+tuanpham96@users.noreply.github.com>
2022-06-21 12:08:02 -0500

    Remove unused file of `02-recog-sim`

--- 02-recog-sim/functions/object_collection/create_squareobj_000.m
+++
@@ -1,7 +0,0 @@
-function X = create_squareobj_000(N)
-X = zeros(N);
-
-X(1:round(N/2),1:round(N/2)) = 1;
-X(round(N/2)+1:end,round(N/2)+1:end) = 1;
-
-end
--- 02-recog-sim/functions/object_collection/create_squareobj_noise.m
+++
@@ -1,3 +0,0 @@
-function X = create_squareobj_noise(N)
-X = randi(2,N) - 1;
-end
